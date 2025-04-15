4643ab28e2066aba11e0f640fcc734e3befc5d55
chenat <109634422+chenat@users.noreply.github.com>
2025-04-15 15:26:57 +0300

    Update build-sh.sh

--- build-sh.sh
+++ build-sh.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# Install system dependencies
 apt-get update
 apt-get install -y --no-install-recommends poppler-utils libcairo2
-
-# Install Python dependencies
 pip install -r requirements.txt
